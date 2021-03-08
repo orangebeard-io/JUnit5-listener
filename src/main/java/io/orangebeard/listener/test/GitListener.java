2427dac3f54577d38c316cf04a69ed28db9949ef
Maarten-Jan van Gool <maarten-jan@orangebeard.io>
2021-03-08 14:16:55 +0100

    [SOJU-676] jgit

--- src/main/java/io/orangebeard/listener/test/GitListener.java
+++ src/main/java/io/orangebeard/listener/test/GitListener.java
@@ -4,9 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.StatusCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
@@ -16,39 +16,47 @@ import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.treewalk.AbstractTreeIterator;
 import org.eclipse.jgit.treewalk.CanonicalTreeParser;
+import org.eclipse.jgit.treewalk.FileTreeIterator;
 
 //https://github.com/centic9/jgit-cookbook
 public class GitListener {
+    private final Repository repository;
+    private final Git git;
 
-    public static void main(String[] args) throws IOException, GitAPIException {
-
+    public GitListener(String dir) throws IOException {
         FileRepositoryBuilder builder = new FileRepositoryBuilder();
-        Repository repository = builder.setGitDir(new File("/home/maarten-jan/code/github-orangebeard/junit5-listener/.git"))
+
+        this.repository = builder.setGitDir(new File(dir))
                 .readEnvironment() // scan environment GIT_* variables
                 .findGitDir() // scan up the file system tree
                 .build();
+        this.git = new Git(repository);
+        System.out.println("Current branch: " + repository.getFullBranch());
 
-        Git git = new Git(repository);
-        StatusCommand status = git.status();
+    }
 
-        // the diff works on TreeIterators, we prepare two for the two branches
-       // ObjectId oldTree = git.getRepository().resolve( "HEAD^{tree}" ); // equals newCommit.getTree()
-        AbstractTreeIterator oldTreeParser = prepareTreeParser(repository, repository.getFullBranch());
-        AbstractTreeIterator newTreeParser = prepareTreeParser(repository, "refs/heads/master");
+    private void printDiffsBetweenCurrentBranchAndMaster() throws IOException, GitAPIException {
+        AbstractTreeIterator currentBranch = prepareTreeParser(repository, repository.getFullBranch());
+        AbstractTreeIterator master = prepareTreeParser(repository, "refs/heads/master");
 
-// then the procelain diff-command returns a list of diff entries
-        List<DiffEntry> diff = new Git(repository).diff().setOldTree(oldTreeParser).setNewTree(newTreeParser).call();
-        for(DiffEntry entry : diff) {
-            System.out.println("Entry: " + entry.getNewPath());
-        }
-
-        for(String uncommitted: status.call().getUncommittedChanges()){
-            System.out.println("uncommitted: "+ uncommitted);
+        List<DiffEntry> diff = git.diff().setOldTree(master).setNewTree(currentBranch).call();
+        for (DiffEntry entry : diff) {
+            System.out.println("Entry: " + entry);
         }
     }
 
+    private void printLocalDiffs() throws IOException, GitAPIException {
+        AbstractTreeIterator currentRepository = new FileTreeIterator( git.getRepository() );
+        AbstractTreeIterator committedChanges = prepareTreeParser(repository, repository.getFullBranch());
 
-    private static AbstractTreeIterator prepareTreeParser(Repository repository, String ref) throws IOException {
+        List<DiffEntry> diff = git.diff().setOldTree(committedChanges).setNewTree(currentRepository).call();
+        for (DiffEntry entry : diff) {
+            System.out.println("Entry: " + entry);
+        }
+
+    }
+
+    private AbstractTreeIterator prepareTreeParser(Repository repository, String ref) throws IOException {
         // from the commit we can build the tree which allows us to construct the TreeParser
         Ref head = repository.exactRef(ref);
         try (RevWalk walk = new RevWalk(repository)) {
@@ -64,5 +72,12 @@ public class GitListener {
 
             return treeParser;
         }
+    }
+
+    public static void main(String[] args) throws IOException, GitAPIException {
+
+        GitListener gitListener = new GitListener("/home/maarten-jan/code/github-orangebeard/junit5-listener/.git");
+        gitListener.printDiffsBetweenCurrentBranchAndMaster();
+        gitListener.printLocalDiffs();
     }
 }
